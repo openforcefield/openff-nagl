451b7bcfcd1f3eda24cdd14bfef878892c7221e5
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-09-20 02:06:06 +1000

    rm old folder

--- gnn_charge_models/nn/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import ConvolutionModule, ReadoutModule
-from .lightning import DGLMoleculeLightningDataModule, DGLMoleculeLightningModel
-from .pooling import PoolAtomFeatures, PoolBondFeatures
