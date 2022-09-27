641368651a7ef9fd3148c9e5879dbd2ae58a54c6
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-09-27 17:12:42 +1000

    add custom encoder

--- openff/nagl/base/base.py
+++ openff/nagl/base/base.py
@@ -1,5 +1,6 @@
 import hashlib
 import inspect
+import pathlib
 import enum
 from typing import Any, ClassVar, Dict, List, Optional, Type, no_type_check
 
@@ -27,6 +28,7 @@ class MutableModel(BaseModel):
             tuple: list,
             unit.Quantity: lambda x: x.to_tuple(),
             enum.Enum: lambda x: x.name,
+            pathlib.Path: str
         }
 
     _hash_fields: ClassVar[Optional[List[str]]] = None
