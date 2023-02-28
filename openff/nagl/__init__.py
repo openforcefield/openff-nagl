1ef5e837270d705c18addbc3a03de0dac205e8c7
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2023-02-28 11:58:14 -0800

    update versioneer

--- openff/nagl/__init__.py
+++ openff/nagl/__init__.py
@@ -6,9 +6,5 @@ A toolkit for the generation of neural network models for predicting molecule pr
 from openff.nagl.nn._models import GNNModel
 
 # Handle versioneer
-from ._version import get_versions
-
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
+from . import _version
+__version__ = _version.get_versions()['version']
