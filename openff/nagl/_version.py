777a1d0bd89f5fac3eff64cc18e9d556c67acf29
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2023-02-28 12:39:55 -0800

    add v to _version

--- openff/nagl/_version.py
+++ openff/nagl/_version.py
@@ -44,7 +44,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "openff-nagl-"
     cfg.versionfile_source = "openff/nagl/_version.py"
     cfg.verbose = False
