4f948ea9956d0b4b876c86a224776f04fcc0856e
Lily Wang <lilywang@lilac-ln02.hpc.private>
2023-07-18 21:20:50 -0400

    add more tests

--- openff/nagl/tests/nn/test_postprocess.py
+++ openff/nagl/tests/nn/test_postprocess.py
@@ -62,6 +62,28 @@ def test_calculate_partial_charges_charged():
     ).reshape((-1, 1))
     assert np.allclose(charges, expected)
 
+@pytest.mark.parametrize(
+    "q0, qi",
+    [
+        (
+            [0.0, 0.0, 0.0, 0.0, 0.0],
+            [-0.22580644, -0.1935484, -0.1935484, -0.1935484, -0.1935484]
+        ),
+        (
+            [-0.04, 0.01, 0.01, 0.01, 0.01],
+            [-0.26580644, -0.1835484, -0.1835484, -0.1835484, -0.1835484]
+        )
+    ]
+)
+def test_regularized_calculate_partial_charges_charged(q0, qi):
+    charges = RegularizedComputePartialCharges._calculate_partial_charges(
+        charge_priors=torch.tensor(q0),
+        electronegativity=torch.tensor([30.8, 27.4, 27.4, 27.4, 27.4]),
+        hardness=torch.tensor([78.4, 73.9, 73.9, 73.9, 73.9]),
+        total_charge=-1.0,
+    ).numpy()
+    expected = np.array(qi).reshape((-1, 1))
+    assert np.allclose(charges, expected)
 
 def test_compute_charges_forward(dgl_methane):
     inputs = torch.tensor(
@@ -82,6 +104,30 @@ def test_compute_charges_forward(dgl_methane):
             0.0088,
             0.0088,
             0.0088,
+        ]
+    ).reshape((-1, 1))
+    assert np.allclose(charges, expected, atol=1e-4)
+
+
+def test_regularized_compute_charges_forward(dgl_methane):
+    inputs = torch.tensor(
+        [
+            [-0.1, 30.8, 78.4],
+            [-0.2, 27.4, 73.9],
+            [0.3, 27.4, 73.9],
+            [-0.5, 27.4, 73.9],
+            [0.5, 27.4, 73.9],
+        ]
+    )
+    charges = RegularizedComputePartialCharges().forward(dgl_methane, inputs)
+    assert np.isclose(charges.sum(), 0.0)
+    expected = np.array(
+        [
+            -0.1351,
+            -0.1912,
+            0.3088,
+            -0.4912,
+            0.5088,
         ]
     ).reshape((-1, 1))
     assert np.allclose(charges, expected, atol=1e-4)
@@ -128,6 +174,53 @@ def test_compute_charges_forward_batched(openff_carboxylate):
             [-0.3543],
             [0.0435],
             [-0.0435],
+        ]
+    )
+
+    assert np.allclose(partial_charges, expected, atol=1e-4)
+
+
+def test_regularized_compute_charges_forward_batched(openff_carboxylate):
+    pytest.importorskip("dgl")
+    dgl_carboxylate = DGLMolecule.from_openff(
+        openff_carboxylate,
+        enumerate_resonance_forms=True,
+        lowest_energy_only=True,
+    )
+    dgl_hcl = DGLMolecule.from_smiles("[H]Cl")
+    batch = DGLMoleculeBatch.from_dgl_molecules([dgl_carboxylate, dgl_hcl])
+    inputs = torch.tensor(
+        [
+            # [H]C(=O)O- form 1
+            [0.1, 30.0, 80.0],
+            [-0.2, 35.0, 75.0],
+            [-0.3, 40.0, 70.0],
+            [0.3, 50.0, 65.0],
+            # [H]C(=O)O- form 2
+            [0.1, 30.0, 80.0],
+            [-0.2, 35.0, 75.0],
+            [0.3, 50.0, 65.0],
+            [-0.3, 40.0, 70.0],
+            # [H]Cl
+            [0.1, 55.0, 60.0],
+            [-0.1, 60.0, 55.0],
+        ]
+    )
+    partial_charges = ComputePartialCharges().forward(batch, inputs)
+    assert partial_charges.shape == (6, 1)
+
+    assert np.isclose(partial_charges.sum(), -1.0)
+    # The carboxylate oxygen charges should be identical.
+    assert np.allclose(partial_charges[2], partial_charges[3])
+
+    expected = np.array(
+        [
+            [-0.3163],
+            [-0.2626],
+            [-0.2105],
+            [-0.2105],
+            [-0.0017],
+            [0.0017],
         ]
     )
 
