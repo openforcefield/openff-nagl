30f961b24d46fc9396a5ca16e6236d32f3a53dde
Lily Wang <lily@mdanalysis.org>
2024-04-05 17:05:58 +1100

    use numpy for masking

--- openff/nagl/molecule/_graph/_graph.py
+++ openff/nagl/molecule/_graph/_graph.py
@@ -8,6 +8,7 @@ from openff.nagl.features.bonds import BondFeature
 from openff.nagl.features._featurizers import AtomFeaturizer, BondFeaturizer
 
 import networkx as nx
+import numpy as np
 import torch
 from ._batch import FrameDict
 
@@ -139,10 +140,13 @@ class NXMolGraph:
         # mask = [x in nodes for x in v]
 
         mask = []
+        # for node in nodes:
+        #     for i_, v_ in enumerate(v):
+        #         if v_ == node:
+        #             mask.append(i_)
+        # mask = []
         for node in nodes:
-            for i_, v_ in enumerate(v):
-                if v_ == node:
-                    mask.append(i_)
+            mask.extend(np.where(v == node)[0])
                     
         U, V, I = u[mask], v[mask], i[mask]
 
