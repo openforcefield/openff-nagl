da3fe192842288380cfcfe77f3b21a71ddd66840
Matt Thompson <matt.thompson@openforcefield.org>
2024-04-05 11:24:43 -0500

    Revert "Add back 3.9 support (#89)"
    This reverts commit 5f771ce49935c1f5cb2798a21da37734c4d5099b.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # Customize MANIFEST.in if the general case does not suit your needs
     # Comment out this line to prevent the files from being packaged with your software
     include_package_data=True,
-    python_requires=">=3.9",          # Python version restrictions
+    python_requires=">=3.10",          # Python version restrictions
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
     # Required packages, pulls from pip if needed
