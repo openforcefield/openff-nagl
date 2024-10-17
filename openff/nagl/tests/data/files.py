882b6c96a1a242fcfbdb464eeb31488f46d0a4f1
Lily Wang <lily@mdanalysis.org>
2024-10-17 15:47:04 +1100

    add test model and tidy files

--- openff/nagl/tests/data/files.py
+++ openff/nagl/tests/data/files.py
@@ -28,7 +28,7 @@ import importlib.resources
 
 data_directory = importlib.resources.files("openff.nagl") / "tests" / "data"
 
-EXAMPLE_AM1BCC_MODEL = data_directory / "example_am1bcc_model.pt"
+EXAMPLE_AM1BCC_MODEL = data_directory / "models" / "example_am1bcc_model.pt"
 EXAMPLE_UNFEATURIZED_PARQUET_DATASET = data_directory / "example-data-labelled-unfeaturized"
 EXAMPLE_FEATURIZED_PARQUET_DATASET = data_directory / "example-data-labelled-featurized"
 EXAMPLE_UNFEATURIZED_PARQUET_DATASET_SHORT = data_directory / "example-data-labelled-unfeaturized-short"
@@ -39,6 +39,6 @@ EXAMPLE_TRAINING_CONFIG_LAZY = data_directory / "example_training_config_lazy.ya
 EXAMPLE_FEATURIZED_LAZY_DATA = data_directory / "cbe6f394311f594a9df33d7580e8b8478f0aef5b505f16f8b2f6af721a14e30d.arrow"
 EXAMPLE_FEATURIZED_LAZY_DATA_SHORT = data_directory / "b6713a9ba87e89cb53d264256664bb9e4e4a5831f0c7660808e9f44a9f832ab5.arrow"
 
-EXAMPLE_MODEL_RC3 = data_directory / "openff-gnn-am1bcc-0.1.0-rc.3.pt"
+EXAMPLE_MODEL_RC3 = data_directory / "models" / "openff-gnn-am1bcc-0.1.0-rc.3.pt"
 
 MOLECULE_ESP_RECORD_CBR3 = data_directory / "training-data" / "molecule-esp-record-cbr3.json"
