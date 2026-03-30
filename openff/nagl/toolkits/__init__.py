28821f7ade0faa08da6a2b25dadf8ddf5c82019f
Jeffrey Wagner <jwagnerjpl@gmail.com>
2026-03-30 13:49:44 -0700

    try to avoid circular import without breaking API

--- openff/nagl/toolkits/__init__.py
+++ openff/nagl/toolkits/__init__.py
@@ -1,6 +1,11 @@
 "Extensions of the OpenFF Toolkit ToolkitWrappers for use with NAGL."
 
+from openff.nagl.toolkits.openeye import NAGLOpenEyeToolkitWrapper
+from openff.nagl.toolkits.rdkit import NAGLRDKitToolkitWrapper
+from openff.nagl.toolkits.registry import NAGLToolkitRegistry
+
 __all__ = [
     "NAGLOpenEyeToolkitWrapper",
     "NAGLRDKitToolkitWrapper",
+    "NAGLToolkitRegistry",
 ]
