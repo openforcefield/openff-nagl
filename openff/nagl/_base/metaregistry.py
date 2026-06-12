7ae6fce389bb0c7927e05bddd89d050ec0c382b1
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-06-12 10:12:07 -0500

    Fix abc/base method sneaking in

--- openff/nagl/_base/metaregistry.py
+++ openff/nagl/_base/metaregistry.py
@@ -51,7 +51,7 @@ def create_registry_metaclass(name_attribute: str = "name", ignore_case: bool = 
             super().__init__(name, bases, namespace, **kwargs)
             if hasattr(cls, cls._key_attribute):
                 attr_val = getattr(cls, cls._key_attribute)
-                if not isinstance(attr_val, property) and attr_val:
+                if not isinstance(attr_val, property) and not callable(attr_val) and attr_val:
                     cls.registry[cls._key_transform(attr_val)] = cls
 
     return RegistryMeta
